3f98a5c3941e2182d95e85a781b17bdb25e34c17
Divyasonawane <134153273+Divyasonawane@users.noreply.github.com>
2024-09-15 20:46:07 +0530

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,12 +1,21 @@
-create index ServicePointCity on ServicePoint(city);
--- queries the address, starttime and endtime of the servicepoints in the same city as a particular user    
--- e.g. SELECT streetaddr,starttime,endtime
---      FROM ServicePoint
---      WHERE ServicePoint.city IN (SELECT Address.city FROM Address WHERE userid=5);
+CREATE INDEX ServicePointCity ON ServicePoint(city);
+--SELECT streetaddr, starttime, endtime
+--FROM ServicePoint
+--WHERE ServicePoint.city IN (
+--  SELECT Address.city 
+--  FROM Address 
+--  WHERE userid = 5);
 
-create index DeliverTime on Deliver_To(TimeDelivered) cluster;
--- queries the ordernumbers that are to be delivered to a particular city during a specified period
--- e.g. select orderNumber 
---      from Deliver_To
---      where Deliver_To.addrid in (select Address.addrid from Address where city = 'Montreal' ) and TimeDelivered < '2017-03-21' and TimeDelivered > '2016-03-21';
+
+CREATE INDEX DeliverTime ON Deliver_To(TimeDelivered);
+
+-- SELECT orderNumber
+-- FROM Deliver_To
+-- WHERE Deliver_To.addrid IN (
+--   SELECT Address.addrid 
+--   FROM Address 
+--   WHERE city = 'Montreal'
+-- )
+-- AND TimeDelivered < '2017-03-21' 
+-- AND TimeDelivered > '2016-03-21';
 
