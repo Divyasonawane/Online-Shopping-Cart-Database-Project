45dc3df9326c6a6508e0250e1996392a754f6c58
Divyasonawane <134153273+Divyasonawane@users.noreply.github.com>
2024-09-15 18:58:44 +0530

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -1,1 +1,306 @@
+insert into users values
+(1 , "Leonard Carroll" , "809-902-4957"),
+(2 , "Sybill C. Kane" , "797-156-7733"),
+(3 , "Ryder Stanton" , "857-833-6279"),
+(4 , "Owen Robbins" , "102-490-9669"),
+(5 , "Tyrone D. Harvey" , "364-220-7833"),
+(6 , "Tanek T. Noble" , "577-561-5445"),
+(7 , "Tanek X. Bridges" , "817-736-8954"),
+(8 , "Latifah Q. Erickson" , "500-413-5229"),
+(9 , "Geoffrey Erickson" , "311-237-5037"),
+(10,'Galvin Hart','501-807-7965'),
+(11,'Nolan Cummings','931-805-0046'),
+(12,'Inga K. Gonzales','736-865-1874'),
+(13,'Orson Dale','997-968-1425'),
+(14,'Rahim Mcdowell','554-339-3873'),
+(15,'Louis Mcguire','725-145-0036'),
+(16,'Kyra Bell','627-635-5250'),
+(17,'Kylee U. Hebert','906-664-4111'),
+(18,'Cora Collins','308-345-2108'),
+(19,'Mara Murray','794-251-3294'),
+(20,'Blair N. Weeks','563-321-0004'),
+(21,'Walter Callahan','568-397-1819'),
+(22,'Brynne V. Hayes','181-495-6507'),
+(23,'Hedy U. Wise','499-991-2743'),
+(24,'Nevada Langley','619-882-8501'),
+(25,'Sasha Church','853-381-3371'),
+(26,'Tamekah Cantu','332-466-8999'),
+(27,'Regan R. Cook','280-150-3915'),
+(28,'Christian England','385-898-0420'),
+(29,'Kevyn P. Cox','768-396-9425'),
+(30,'Leah B. Franklin','853-688-0957'),
+(31,'Urielle Humphrey','926-331-0012'),
+(32,'Kendall Lane','661-911-8866'),
+(33,'Ezra Randolph','922-755-8559'),
+(34,'Ashton Maddox','523-845-2705'),
+(35,'Sopoline Spence','614-499-4578'),
+(36,'Justin Guerrero','516-139-5882'),
+(37,'Phoebe S. Wynn','264-976-8713'),
+(38,'Noble R. Flynn','501-240-1875'),
+(39,'Buffy S. Chandler','859-284-1514'),
+(40,'Finn Hood','183-267-5415'),
+(41,'Alea V. Brewer','482-150-4868'),
+(42,'Elvis M. Watkins','634-901-1779'),
+(43,'Shafira T. Frank','483-164-3378'),
+(44,'Beck C. Herman','738-418-4743'),
+(45,'Minerva V. William','539-390-0106'),
+(46,'Dahlia Walls','953-437-1177'),
+(47,'Travis Savage','640-362-9129'),
+(48,'Illana Gallegos','762-486-9320'),
+(49,'Sylvia N. Santos','375-164-3524'),
+(50,'Baxter O. May','346-460-2655');
+
+INSERT INTO buyer VALUES (5);
+INSERT INTO buyer VALUES (41);
+INSERT INTO buyer VALUES (18);
+INSERT INTO buyer VALUES (12);
+INSERT INTO buyer VALUES (24);
+INSERT INTO buyer VALUES (16);
+INSERT INTO buyer VALUES (17);
+INSERT INTO buyer VALUES (85);
+INSERT INTO buyer VALUES (56);
+INSERT INTO buyer VALUES (66);
+INSERT INTO buyer VALUES (33);
+INSERT INTO buyer VALUES (28);
+INSERT INTO buyer VALUES (9);
+
+INSERT INTO seller VALUES (1);
+INSERT INTO seller VALUES (15);
+INSERT INTO seller VALUES (30);
+INSERT INTO seller VALUES (32);
+INSERT INTO seller VALUES (61);
+INSERT INTO seller VALUES (63);
+INSERT INTO seller VALUES (10);
+INSERT INTO seller VALUES (9);
+INSERT INTO seller VALUES (4);
+INSERT INTO seller VALUES (88);
+
+insert into address values
+(1,28,'Haviva F. Davidson','915-791-0670','Ontario','Ottawa-Carleton','8484 Nonummy Av.','N0C 7E0'),
+(2,21,'Rae R. Lara','495-472-7273','Ontario','St. Catharines','5836 Iaculis Avenue','L7M 2V6'),
+(3,36,'Ashely O. Knowles','895-493-8279','British Columbia','Hudson''s Hope','P.O. Box 827, 1825 Fusce Ave','V7A 5A6'),
+(4,11,'Hollee G. Hudson','516-753-9880','Quebec','Baie-Comeau','9376 Imperdiet Street','G4J 8Y2'),
+(5,39,'Kylynn G. Waller','838-912-3476','Quebec','Neuville','Ap #179-5810 Nunc Rd.','G9K 8Y6'),
+(6,38,'Audra Manning','898-140-2501','Manitoba','Daly','732-9599 Lacinia Rd.','R1X 3Z2'),
+(7,9,'Zia Guzman','781-454-1244','Ontario','Cornwall','Ap #553-1602 Sit Av.','M4G 2Y2'),
+(8,12,'Xerxes Luna','841-769-6638','Quebec','Verdun','P.O. Box 210, 6443 Donec Road','G5L 7P4'),
+(9,25,'Azalia P. Hooper','116-526-3919','Quebec','Lachine','Ap #379-4986 Odio. Rd.','H3P 9J4'),
+(10,32,'Dahlia Rojas','916-981-2927','Ontario','Vaughan','Ap #642-6556 Adipiscing St.','L4T 4A3'),
+(11,11,'Melanie Duran','440-799-4382','Ontario','Tay','565-8573 Et Road','M0K 5N9'),
+(12,19,'Nyssa S. Maynard','861-136-6572','Prince Edward Island','Montague','P.O. Box 794, 5616 Gravida Rd.','C5Z 2Z0'),
+(13,38,'Hilary I. Boone','618-129-6884','British Columbia','Sooke','P.O. Box 594, 5086 At Avenue','V9W 0S9'),
+(14,17,'Aileen B. Pratt','785-162-9219','Quebec','Montreal','654-6775 Primis Av.','G7H 5E9'),
+(15,14,'Rudyard O. Kent','896-514-6693','Alberta','Wood Buffalo','271-2092 Enim. Av.','T6E 3R8'),
+(16,24,'Quentin Lewis','616-464-2316','Quebec','Pointe-au-Pic','P.O. Box 331, 348 Ut Rd.','J7M 7X5'),
+(17,33,'Alan A. Norton','229-829-6509','Ontario','Ottawa','700-385 Augue, Road','K9C 5N6'),
+(18,11,'Sigourney U. Stevens','206-582-2529','Alberta','Lamont','7636 Felis, Av.','T5X 9Y0'),
+(19,34,'Kevyn Hines','146-355-8639','Alberta','Sylvan Lake','578-8255 Amet Rd.','T4N 2R9'),
+(20,34,'Tyler O. Curry','111-353-8652','Quebec','Baie-Comeau','171-701 Semper Street','H3H 0J1'),
+(21,32,'Nola Y. Mcmahon','826-961-6244','Saskatchewan','Calder','477-5078 Dolor Street','S2X 2E7'),
+(22,34,'Drake Witt','464-747-5669','Nova Scotia','Pugwash','6574 Ipsum Av.','B9S 5E9'),
+(23,39,'Valentine Y. Peterson','175-217-1821','Alberta','Westlock','Ap #338-3056 Justo. St.','T5S 5V8'),
+(24,20,'Daphne Munoz','841-264-2751','Ontario','Midlands','7408 Pharetra Rd.','N8M 0B3'),
+(25,34,'Thomas O. Morse','700-652-8089','Saskatchewan','Estevan','P.O. Box 537, 4062 Mauris Ave','S0H 8L8'),
+(26,15,'Dahlia H. Fox','747-633-8010','Ontario','Russell','Ap #248-8859 Feugiat. Street','L6V 8K8'),
+(27,11,'Brian E. Cannon','305-973-3594','Quebec','Lévis','Ap #612-3420 Montes, Road','J4R 1Y8'),
+(28,13,'Cullen Vega','465-172-3435','Ontario','Burlington','P.O. Box 881, 6715 Dignissim Ave','K7M 8N4'),
+(29,20,'Bruce U. Blair','899-466-5745','Manitoba','Beausejour','P.O. Box 821, 8801 Diam Street','R9G 4Y7'),
+(30,6,'Ivan L. Barrett','626-422-8515','Ontario','St. Catharines','Ap #713-8042 Sit Street','K4N 0L5'),
+(31,34,'Jared X. Leonard','271-473-5902','Alberta','Rocky Mountain House','P.O. Box 900, 5389 Sagittis Avenue','T1C 1C5')
+(32,37,'Daryl Terry','498-809-2944','Ontario','Quinte West','P.O. Box 960, 527 Tristique Avenue','K9C 4V9'),
+(33,39,'Melodie Reynolds','111-137-4809','Quebec','Valcourt','513-8244 Elit, Street','J2B 9Y6'),
+(34,29,'Robert Whitley','381-319-3443','Manitoba','Flin Flon','P.O. Box 896, 5501 Mauris Rd.','R6B 5B6'),
+(35,12,'Maggie B. Mccoy','230-943-0150','Ontario','St. Catharines','P.O. Box 996, 7765 Hendrerit Ave','N3H 8X2'),
+(36,32,'Isaiah X. Montoya','410-539-2283','Ontario','Grey County','274-9821 Consectetuer Road','N9X 1V2'),
+(37,30,'Fiona Ruiz','964-373-1028','Quebec','Dorval','Ap #396-5126 Ut Road','G1T 6B6'),
+(38,39,'Bree Bray','717-977-7726','Ontario','Scarborough','8565 Etiam St.','L5G 8K0'),
+(39,5,'TaShya Y. Buckley','779-640-3249','Quebec','Saint-Georges','Ap #881-9739 In Rd.','G0L 4E2'),
+(40,9,'Jason N. Contreras','454-369-2951','Quebec','Price','Ap #483-4891 In Ave','G4A 0Z4')
+(41,30,'Alea A. Ortiz','717-521-9609','Alberta','Penhold','935-227 Dis St.','T4W 9W2'),
+(42,10,'Rhonda C. Luna','374-193-9903','Ontario','Kapuskasing','P.O. Box 990, 2280 Blandit. Rd.','K3C 7R1'),
+(43,18,'Ashton Sykes','687-799-5559','Prince Edward Island','Stratford','4199 Mollis. Av.','C1A 6P3'),
+(44,32,'Roanna Guy','412-193-7026','Quebec','Alma','2484 At St.','G0C 5W1'),
+(45,35,'Judah Wagner','982-952-2901','Newfoundland and Labrador','Marystown','537-9791 In St.','A0S 5J4'),
+(46,1,'Kimberley O. Taylor','992-637-8656','Quebec','Dubuisson','8844 Mollis Av.','J4Y 6J7'),
+(47,36,'Kieran Y. Sutton','239-332-3013','Alberta','Breton','Ap #980-7807 Morbi Av.','T5Y 2C4'),
+(48,11,'Audra Delacruz','335-263-4916','Newfoundland and Labrador','Fortune','Ap #640-8358 Nullam Rd.','A0S 1T7'),
+(49,5,'Lance Weeks','988-570-1631','Nova Scotia','Cape Breton Island','5297 Ante Street','B0V 6N6'),
+(50,15,'Lydia I. Cannon','326-839-5531','Ontario','Malahide','P.O. Box 155, 6071 Phasellus Ave','K9S 3K6');
+
+insert into bankcard values
+('5872 8293 3321 0123', '2020-5-14','BMO'),
+('4902 9212 3402 8831', '2027-7-01','CIBC'),
+('5782 7293 8472 0318', '2028-8-15','CIBC'),
+('4509 4462 9302 0716', '2030-1-30','CIBC'),
+('8473 9299 8492 7361', '2024-6-20','BMO'),
+('4457 4992 7391 0219', '2025-8-30','BMO'),
+('5728 8391 7362 5381', '2035-5-20','CIBC'),
+('7283 8982 8361 5593', '2029-4-14','BMO'),
+('9238 2749 5738 5921', '2027-8-01','CIBC'),
+('2839 8219 3728 2933', '2031-5-20','CIBC');
+
+insert into creditcard values
+('4902 9212 3402 8831',5,'VISA'),
+('4023 1231 3431 8623',18,'MASTER'),
+('8473 9299 8492 7361',41,'VISA'),
+('7283 8982 8361 5593',24,'VISA'),
+('9238 2749 5738 5921',12,'MASTER'),
+('2839 8219 3728 2933',9,'VISA');
+
+insert into debitcard values
+('5872 8293 3321 0123', 15),
+('5728 8391 7362 5381', 30),
+('4457 4992 7391 0219', 32),
+('5782 7293 8472 0318', 1),
+('4509 4462 9302 0716', 3);
+
+insert into store values
+(8,'Choby Collection','Quebec','Montreal','No.2012 Sanguinet',4,'2006-5-30'),
+(10,'BestBuy','British Columbia','Vancouver','No.20 ShineStreet',5,'2002-8-05'),
+(39,'CoolGuy','Quebec','Montreal','No.1023 St-Catherine',3,'2012-2-24'),
+(77,'Your Favorite','Ontario','Toronto','No.3022 Saint Denis',4,'2015-7-25'),
+(218,'American Eagle','Quebec','Quebec city','No.5 Berri Street',2,'2004-11-17');
+
+insert into brand values
+('Asus'),('Bose'), ('Canon') , ('DELL') , ('GoPro') , ('Microsoft');
+
+insert into servicepoint values
+(27,'2045 Sanguinet Street','Montreal','Quebec','10:00 am','5:30 pm'),
+(72,'37 Sherbrook Street','Montreal','Quebec','9:30 am','5:30 pm'),
+(34,'45 St Denis Street','Montreal','Quebec','10:00 am','6:00 pm'),
+(19,'1099 University street','Montreal','Quebec','10:00 am','5:30 pm'),
+(9,'27 St Catherine Street','Quebec City','Quebec','10:00 am','5:30 pm');
+
+insert into product values
+(1,8,'Asus','ASUS Chromebook 11.6 laptop','laptop','C201PA-DS02','navy blue',10,262),
+(2,10,'Bose','Bose QuietComfort 35 wireless headphone','headphone','759944-0010','black',100,449),
+(3,39,'Canon','Canon EOS Rebel T5','cameras','9126B003','black',50,500),
+(4,77,'DELL','DELL Ultra HD 4k Monitor P2715Q 27-Inch Screen LED-Lit Monitor','computer accessories','P2715Q','black',40,713),
+(5,218,'GoPro','GoPro HERO5','cameras','CHDHX-501-CA','black',80,599),
+(6,218,'Microsoft','Microsoft Xbox 360 Wired Controller for Windows','controllers','52A-00004','black',60,35),
+(7,8,'Microsoft','Microsoft Sculpt Ergonomic Wireless Bluetrack Desktop Keyboard','computer accessories','L5V-00003','black',70,126),
+(8,8,'Microsoft','Microsoft Surface Pro 4 i5 (128GB) with Wireless Media Adapter','laptop','CR5-00001','black',30,1350);
+
+insert into orderItem values
+(1,1,262,'2016-09-18'),
+(2,2,449,'2014-03-22'),
+(3,3,500,'2017-01-23'),
+(4,4,713,'2017-01-23'),
+(5,5,599,'2016-12-27'),
+(6,6,35,'2015-05-30'),
+(7,7,126,'2017-02-12'),
+(8,8,1350,'2016-12-27'),
+(9,1,262,'2015-02-13'),
+(10,5,599,'2015-08-22');
+
+insert into comments values
+('2017-02-21',5,1,4.3,'The laptop works amazingly. It holds a 10 hour charge, is compact to wander with, the brightness/volume features are perfect.'),
+('2016-09-18',5,2,4.6,'These headphones are worth the money, yes even the CAD price. They sound good and the noise cancellation is incredible.'),
+('2014-03-22',12,3,4.7,'Someone always has a better camera. That being said, this is an admirable performer with enough features for most.'),
+('2017-01-23',12,4,4.8,'This monitor is simply amazing. My eyes are not getting any younger and this makes everything very crisp and clear. I can definitely notice the improvement over a 2560X1440 display. I am ordering another one!'),
+('2016-12-27',18,5,4.3,'It is the first Go Pro I have had and so far I am loving it, the voice control is great for when you have your hands busy or can not reach the buttons. The apps for camera pairing and video editing are just great as well'),
+('2015-05-30',24,6,4.5,'Works flawlessly. After plugging it in, Windows automatically installed the drivers for it, and it was working in a matter of moments. It is an actual Xbox controller so it feels solid. This was definitely a purchase I would make again.'),
+('2017-02-12',41,7,4.3,'Overall good keyboard and mouse. However the moment your USB receiver dies, the whole thing goes in the garbage.'),
+('2016-12-27',41,8,3.8,'I love my surface. I got it a couple of weeks ago. I amm a life time mac user, but I consider myself quite tech savvy. I think this is a great device.'),
+('2015-05-30',12,2,4.3,'I have been using it for a week now. For a short conclusion, i love the headset.'),
+('2015-08-22',18,1,5,'This was definitely an impulse buy on my part but has turned out to be one of the best things I have ever invested in for school!'),
+('2015-08-22',24,5,5,'First go pro I have ever purchased. Really impressed with the quality and ease of use. The stabilizer is awesome, do not need to warp stabilize a ton in adobe.');
+
+insert into  save_to_shopping_cart values 
+(5,1,'2017-02-21',1),
+(5,2,'2016-09-18',1),
+(18,3,'2014-03-22',2),
+(12,4,'2017-01-23',1),
+(24,5,'2016-12-27',2),
+(41,6,'2015-05-30',1),
+(9,7,'2016-02-19',4),
+(18,8,'2012-01-20',1);
+
+insert into manage values
+(1,8,'2015-12-24'),
+(15,10,'2006-03-20'),
+(30,39,'2006-08-09'),
+(32,77,'2005-03-04'),
+(1,218,'2010-05-06');
+
+insert into after_sales_service_at values
+('Asus',27),
+('Microsoft',27),
+('DELL',72),
+('Microsoft',34),
+('Bose',34),
+('Microsoft',19),
+('Canon',19),
+('Microsoft',9),
+('DELL',9),
+('GoPro',9);
+
+insert into orders values
+(76023921,'Paid','2016-09-18',315),
+(23924831,'Paid','2014-03-22',174),
+(87291231,'Unpaid','2017-01-23',102),
+(58293123,'Unpaid','2016-12-27',928),
+(12992012,'Paid','2015-05-30',203),
+(59203012,'Paid','2017-02-12',87),
+(91027482,'Unpaid','2016-12-27',187),
+(72839301,'Unpaid','2015-02-13',293),
+(93042135,'Paid','2015-08-22',52),
+(74892932,'UnPaid','2016-02-19',77),
+(83921201,'Paid','2017-01-03',301),
+(73919231,'Paid','2014-02-11',20),
+(84821231,'Paid','2014-11-02',18);
+
+insert into contain values
+(76023921,1,1),
+(76023921,2,2),
+(23924831,3,1),
+(87291231,4,2),
+(58293123,5,1),
+(12992012,6,1),
+(12992012,7,1),
+(59203012,8,1),
+(91027482,9,1);
+(72839301,10,1),
+(93042135,1,1),
+(93042135,2,1),
+(93042135,3,1),
+(82921201,4,4),
+(84821231,5,3),
+(73919231,6,2),
+(83921201,7,5);
+
+INSERT INTO deliver_to VALUES
+(39,87291231,'2017-02-17'),
+(1,12992012,'2015-6-10'),
+(7,76023921,'2015-10-04'),
+(12,59203012,'2017-02-20'),
+(14,93042135,'2015-09-08');
+
+insert into payment values 
+(23924831,'4902 9212 3402 8831','2016-08-23'),
+(87291231,'4023 1231 3431 8623','2016-08-07'),
+(58293123,'8473 9299 8492 7361','2016-09-07'),
+(12992012,'7283 8982 8361 5593','2015-02-23'),
+(59203012,'9238 2749 5738 5921','2013-09-07'),
+(91027482,'4902 9212 3402 8831','2017-02-01'),
+(72839301,'4902 9212 3402 8831','2016-10-23'),
+(93042135,'4023 1231 3431 8623','2016-10-07'),
+(91027482,'7283 8982 8361 5593','2017-02-01'),
+(84821231,'9238 2749 5738 5921','2017-01-23'),
+(73919231,'9238 2749 5738 5921','2016-10-23'),
+(83921201,'9238 2749 5738 5921','2017-02-01');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
